420d2055f7eea0c546e2e22e242d2f9801319b9b
Kirill Vasilev <kirill.vasilev.work@gmail.com>
2021-07-30 17:25:16 +0300

    Initial commit

---
+++ src/main/java/com/kirillvasilev/spring/springboot/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.kirillvasilev.spring.springboot.service;
+
+
+import com.kirillvasilev.spring.springboot.dao.EmployeeDAO;
+import com.kirillvasilev.spring.springboot.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
